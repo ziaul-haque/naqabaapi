af3d1a682398c822a11d07bedb85e1f6801effdf
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-03 16:00:05 +0530

    Params added

--- resources/reports.py
+++ resources/reports.py
@@ -1,10 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, request
 
 from reports.common import made_raw_sql_query
 
 
 class Companies(Resource):
     def get(self):
+        args = request.args
         result = made_raw_sql_query("select * from paths")
 
         if not result:
