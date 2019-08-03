c94f61607ed6a2d945ec1de6f02a358fbff14dc4
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-03 21:45:57 +0530

    Conn string updated

--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
 import pyodbc
 
-db_con = pyodbc.connect('Driver={SQL Server};Server=localhost;Database=daif-avl-db;UID=sa;PWD=Password01')
+db_con = pyodbc.connect('Driver={SQL Server};Server=ec2-18-191-218-21.us-east-2.compute.amazonaws.com;Database=daifdev;UID=sa;PWD=Naqaba@password01')
 
