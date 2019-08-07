09672915863a1944192db2b7ba2e1d96ba47642c
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-08 01:18:23 +0530

    Development started

--- Model.py
+++ Model.py
@@ -1,6 +1,5 @@
 import pyodbc
 db_con = pyodbc.connect('Driver={SQL Server};Server=ec2-18-191-218-21.us-east-2.compute.amazonaws.com;Database=daif-avl-db;UID=sa;PWD=Naqaba@password01')
-#db_con = pyodbc.connect('Driver={SQL Server};Server=localhost;Database=daif-avl-db;UID=sa;PWD=Password01')
 #pyodbc.connect('DRIVER=FreeTDS;SERVER=<IP_OR_HOSTNAME>;PORT=1433;DATABASE=<DATABASE_NAME>;UID=<USERNAME>;PWD=<PASSWORD>;TDS_Version=8.0;')
 
 
