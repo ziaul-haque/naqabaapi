42bbf0d45e37cc2d73b57a73310a7626302b140d
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-24 17:28:26 +0530

    Hard coded arabic test

--- resources/Hello.py
+++ resources/Hello.py
@@ -1,6 +1,13 @@
 from flask_restful import Resource
-
+import json
+from flask import Response
 
 class Hello(Resource):
     def get(self):
-        return {"message": "Congratulations, You are successfully configured naqaba API"}
+        str = 'الدول الأفريقية غير العربية'
+        json_obj = {'data': str}
+        json_response = json.dumps(json_obj, ensure_ascii=False)
+        uc_response = json_response.encode('UTF-8')
+        return Response(uc_response, content_type="application/json; charset=utf-8", mimetype="application/json;")
+        # return {'data': str.encode('utf-16')}
+        # return {"message": "Congratulations, You are successfully configured naqaba API"
