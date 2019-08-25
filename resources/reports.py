15333a946dafdf2a16599220147a5a0384478834
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-25 13:24:08 +0530

    removed status

--- resources/reports.py
+++ resources/reports.py
@@ -19,24 +19,24 @@ class Test(Resource):
 class Companies(Resource):
     def get(self):
         result = made_database_stored_procedure_query('sp_get_transport_companies', [])
-        return result, status.HTTP_200_OK
+        return result
 
 
 class Vehicles(Resource):
     def get(self):
         args = request.args
         result = made_database_stored_procedure_query('sp_get_transport_vehicles', args)
-        return result, status.HTTP_200_OK
+        return result
 
 
 class Mosasas(Resource):
     def get(self):
         result = made_database_stored_procedure_query('sp_get_active_mosasa', [])
-        return result, status.HTTP_200_OK
+        return result
 
 
 class Locations(Resource):
     def get(self):
         args = request.args
         result = made_database_stored_procedure_query('sp_get_vehicle_location', args)
-        return result, status.HTTP_200_OK
+        return result
