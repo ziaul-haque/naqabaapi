186951a8609d74917fe2d0ed9b3a1cacd9441bc0
Ziaul Haque <ziaul.ali@gmail.com>
2019-08-25 13:02:30 +0530

    Removed redundant code'

--- resources/reports.py
+++ resources/reports.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, request
 from flask import Response
+from flask_api import status
 
 import json
 
@@ -18,39 +19,24 @@ class Test(Resource):
 class Companies(Resource):
     def get(self):
         result = made_database_stored_procedure_query('sp_get_transport_companies', [])
-        json_obj = {'companies': result}
-        json_response = json.dumps(json_obj, ensure_ascii=False)
-        uc_response = json_response.encode('UTF-8')
-        return Response(uc_response)
+        return result, status.HTTP_200_OK
 
 
 class Vehicles(Resource):
     def get(self):
         args = request.args
         result = made_database_stored_procedure_query('sp_get_transport_vehicles', args)
-
-        json_obj = {'vehicles': result}
-        json_response = json.dumps(json_obj, ensure_ascii=False)
-        uc_response = json_response.encode('UTF-8')
-        return Response(uc_response)
+        return result, status.HTTP_200_OK
 
 
 class Mosasas(Resource):
     def get(self):
         result = made_database_stored_procedure_query('sp_get_active_mosasa', [])
-
-        json_obj = {'mosasas': result}
-        json_response = json.dumps(json_obj, ensure_ascii=False)
-        uc_response = json_response.encode('UTF-8')
-        return Response(uc_response, content_type="application/json; charset=utf-8", mimetype="application/json;")
+        return result, status.HTTP_200_OK
 
 
 class Locations(Resource):
     def get(self):
         args = request.args
         result = made_database_stored_procedure_query('sp_get_vehicle_location', args)
-
-        json_obj = {'locations': result}
-        json_response = json.dumps(json_obj, ensure_ascii=False)
-        uc_response = json_response.encode('utf-8')
-        return Response(uc_response, content_type="application/json; charset=utf-8", mimetype="application/json;")
+        return result, status.HTTP_200_OK
